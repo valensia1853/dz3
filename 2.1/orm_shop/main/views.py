b1a1ad31d9f8a849e17df03e9ae0fb238fef600c
batsokin93 <152905867+valensia1853@users.noreply.github.com>
2024-08-25 01:22:02 +0300

    Update views.py

--- 2.1/orm_shop/main/views.py
+++ 2.1/orm_shop/main/views.py
@@ -1,25 +1,26 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from main.models import Car
+from .models import Car, Sale
+
 
 
 def cars_list_view(request):
     # получите список авто
+    cars = Car.objects.all()
     template_name = 'main/list.html'
-    return render(request, template_name, {})  # передайте необходимый контекст
+    return render(request, template_name, {'cars': cars})  # передайте необходимый контекст
 
 
 def car_details_view(request, car_id):
     # получите авто, если же его нет, выбросьте ошибку 404
+    car = get_object_or_404(Car, id=car_id)
     template_name = 'main/details.html'
-    return render(request, template_name, {})  # передайте необходимый контекст
+    return render(request, template_name, {'car': car})  # передайте необходимый контекст
 
 
 def sales_by_car(request, car_id):
-    try:
-        # получите авто и его продажи
-        template_name = 'main/sales.html'
-        return render(request, template_name, {})  # передайте необходимый контекст
-    except Car.DoesNotExist:
-        raise Http404('Car not found')
+    car = get_object_or_404(Car, id=car_id)
+    sales = Sale.objects.filter(car=car)
+    template_name = 'main/sales.html'
+    return render(request, template_name, {'car': car, 'sales': sales})
